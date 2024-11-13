6bc50f5cd018b4c223d037940682be1ca936cc74
piro-piyush <piyush72717272@gmail.com>
2024-11-13 15:07:02 +0530

    loading animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animation/explicit_examples/list_animation.dart';
+import 'package:animation/explicit_examples/loading_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ListAnimation(),
+      home: const RadialProgressAnimation(progress: 0.55, color: Colors.blue),
     );
   }
 }
