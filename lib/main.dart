57d87c9ec7543ed4091fa8349e4c780c8a8e63df
piro-piyush <piyush72717272@gmail.com>
2024-11-13 16:04:12 +0530

    custom paint animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animation/custom_painter_example/bouncing_ball_animation.dart';
 import 'package:animation/page_route_builder_animation/splash_animation.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SplashAnimation());
+        home: const BouncingBallAnimation());
   }
 }
