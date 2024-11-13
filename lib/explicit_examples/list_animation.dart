1126de4c1ecab5571054a96e6964209fd0216c96
piro-piyush <piyush72717272@gmail.com>
2024-11-13 12:40:55 +0530

    list animation

--- lib/explicit_examples/list_animation.dart
+++ lib/explicit_examples/list_animation.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ListAnimation extends StatelessWidget {
+class ListAnimation extends StatefulWidget {
   const ListAnimation({super.key});
+
+  @override
+  State<ListAnimation> createState() => _ListAnimationState();
+}
+
+class _ListAnimationState extends State<ListAnimation>
+    with SingleTickerProviderStateMixin {
+  int itemCount = 5;
+  late AnimationController animationController;
+  late List<Animation<Offset>> sliderAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    sliderAnimation = List.generate(
+      itemCount,
+      (index) => Tween(begin: const Offset(-1, 0), end: Offset.zero).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: Interval(index * (1 / itemCount), 1),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +36,24 @@ class ListAnimation extends StatelessWidget {
         title: const Text('List Animation'),
       ),
       body: ListView.builder(
-        itemCount: 5,
+        itemCount: itemCount,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Hello World, Rivaan. ${index.toString()}'),
+          return SlideTransition(
+            position: sliderAnimation[index],
+            child: ListTile(
+              title: Text('Hello World, Rivaan. ${index.toString()}'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if (animationController.isCompleted) {
+            animationController.reverse();
+          } else {
+            animationController.forward();
+          }
+        },
         child: const Icon(Icons.done),
       ),
     );
